4676b290601d0ab148fef6f2e4b0f79afc985893
dennis maftulyak <maft.den@gmail.com>
2018-11-22 07:37:24 +0200

    data storage change

--- src/components/info.js
+++ src/components/info.js
@@ -1,188 +1,222 @@
-export const categorys = ["Арт", "Природа", "Города", "Еда и напитки", "Прозрачные"];
-
-export const subCategorys = {
-    0: ["Арт-Париж", "Кафе", "Италия", "Плитка", "Серые волны", "Меню"],
-    1: ["Алтай", "Пляж", "Океан", "Волки", "Риф", "Камни", "Лошади"],
-    2: ["Город на реке", "Сан-Франциско", "Стамбул Акварель", "Старый мост"],
-    3: ["Лайм", "Вино с бокалом"],
-    4: ["Темыне кирпичи", "Серая стена", "Светлые кирпичи"]
-};
-
-export const clientInfo = {
-    "00": {
+export const data = [
+    {
         id: 1,
+        categoryName: "Арт",
+        subCategoryName: "Арт-Париж",
         address: "ул. Пушкинская, д.9",
         area: "2,4 м. кв.",
         glassType: "Осветленное стекло",
         price: "24 300",
         img: "a1.jpg"
     },
-    "01": {
+    {
         id: 2,
+        categoryName: "Арт",
+        subCategoryName: "Кафе",
         address: "ул. Мосфильмовская, д.12",
         area: "2,6 м. кв.",
         glassType: "Осветленное стекло",
         price: "25 100",
         img: "a2.jpg" 
     },
-    "02": {
+    {
         id: 3,
+        categoryName: "Арт",
+        subCategoryName: "Италия",
         address: "Видное, б-р Зеленые аллеи д.3",
         area: "1,9 м. кв.",
         glassType: "Осветленное стекло",
         price: "19 550",
         img: "a3.jpg" 
     },
-    "03": {
+    {
         id: 4,
+        categoryName: "Арт",
+        subCategoryName: "Плитка",
         address: "Красногорск, ул. Головкина д. 7",
         area: "2,8 м. кв.",
         glassType: "Осветленное стекло",
         price: "26 300",
         img: "a4.jpg" 
     },
-    "04": {
+    {
         id: 5,
+        categoryName: "Арт",
+        subCategoryName: "Серые волны",
         address: "ул. Новотушинская, д.5",
         area: "2,1 м. кв.",
         glassType: "Осветленное стекло",
         price: "20 100",
         img: "a5.jpg" 
     },
-    "05": {
+    {
         id: 6,
+        categoryName: "Арт",
+        subCategoryName: "Меню",
         address: "ул. Борисовские пруды, д.14",
         area: "2 м. кв.",
         glassType: "Классическое стекло",
         price: "16 430",
         img: "a6.jpg" 
     },
-    "10": {
+    {
         id: 7,
+        categoryName: "Природа",
+        subCategoryName: "Алтай",
         address: "Ленинградский проспект, д.14",
         area: "1,9 м. кв.",
         glassType: "Классическое стекло",
         price: "16 050",
         img: "p1.jpg"
     },
-    "11": {
+    {
         id: 8,
+        categoryName: "Природа",
+        subCategoryName: "Пляж",
         address: "ЖК Суханово-Парк, д.1",
         area: "3,4 м. кв.",
         glassType: "Осветленное стекло",
         price: "29 350",
         img: "p2.jpg" 
     },
-    "12": {
+    {
         id: 9,
+        categoryName: "Природа",
+        subCategoryName: "Океан",
         address: "Химки, ул. 9 мая, д. 21",
         area: "2,1 м. кв.",
         glassType: "Осветленное стекло",
         price: "21 100",
         img: "p3.jpg" 
     },
-    "13": {
+    {
         id: 10,
+        categoryName: "Природа",
+        subCategoryName: "Волки",
         address: "ул. Коломенская д. 12/2",
         area: "2,1 м. кв.",
         glassType: "Классическое стекло",
         price: "17 900",
         img: "p4.jpg" 
     },
-    "14": {
+    {
         id: 11,
+        categoryName: "Природа",
+        subCategoryName: "Риф",
         address: "ЖК Мелодия леса, д.7",
         area: "2,9 м. кв.",
         glassType: "Осветленное стекло",
         price: "26 300",
         img: "p5.jpg" 
     },
-    "15": {
+    {
         id: 12,
+        categoryName: "Природа",
+        subCategoryName: "Камни",
         address: "Балашиха, ул. Добросельская, д. 14",
         area: "3 м. кв.",
         glassType: "Осветленное стекло",
         price: "26 450",
         img: "p6.jpg" 
     },
-    "16": {
+    {
         id: 13,
+        categoryName: "Природа",
+        subCategoryName: "Лошади",
         address: "Рублевское шоссе, д. 21",
         area: "2,1 м. кв.",
         glassType: "Осветленное стекло",
         price: "23 700",
         img: "p7.jpg" 
     },
-    "20": {
+    {
         id: 14,
+        categoryName: "Города",
+        subCategoryName: "Город на реке",
         address: "ул. Череповецкая, д. 24",
         area: "2,6 м. кв.",
         glassType: "Осветленное стекло",
         price: "25 000",
         img: "g1.jpg"  
     },
-    "21": {
+    {
         id: 15,
+        categoryName: "Города",
+        subCategoryName: "Сан-Франциско",
         address: "Подольск, ул. Литейная, д. 42а",
         area: "2,5 м. кв.",
         glassType: "Классическое стекло",
         price: "21 900",
         img: "g2.jpg"   
     },
-    "22": {
+   {
         id: 16,
+        categoryName: "Города",
+        subCategoryName: "Стамбул Акварель",
         address: "ул. Коненкова, д. 6",
         area: "2 м. кв.",
         glassType: "Классическое стекло",
         price: "18 150",
         img: "g3.jpg"   
     },
-    "23": {
+    {
         id: 17,
+        categoryName: "Города",
+        subCategoryName: "Старый мост",
         address: "ул. Рождественка, д. 19",
         area: "2,5 м. кв.",
         glassType: "Осветленное стекло",
         price: "24 040",
         img: "g4.jpg"   
     },
-    "30": {
+    {
         id: 18,
+        categoryName: "Еда и напитки",
+        subCategoryName: "Лайм",
         address: "Химки, ул. Озерная, д. 4",
         area: "2,5 м. кв.",
         glassType: "Классическое стекло",
         price: "20 980",
         img: "e1.jpg"   
     },
-    "31": {
+    {
         id: 19,
+        categoryName: "Еда и напитки",
+        subCategoryName: "Вино с бокалом",
         address: "пр-т. Вернадского, д. 67",
         area: "1,8 м. кв.",
         glassType: "Осветленное стекло",
         price: "19 200",
         img: "e2.jpg"   
     },
-    "40": {
+    {
         id: 20,
+        categoryName: "Прозрачные",
+        subCategoryName: "Темыне кирпичи",
         address: "Одинцово, Можайское шоссе, д. 136а",
         area: "2,1 м. кв.",
         glassType: "Осветленное стекло",
         price: "14 300",
         img: "c1.jpg"   
     },
-    "41": {
+    {
         id: 21,
+        categoryName: "Прозрачные",
+        subCategoryName: "Серая стена",
         address: "ул. Лескова, д. 19",
         area: "2,3 м. кв.",
         glassType: "Осветленное стекло",
         price: "15 700",
         img: "c2.jpg"   
     },
-    "42": {
+    {
         id: 22,
+        categoryName: "Прозрачные",
+        subCategoryName: "Светлые кирпичи",
         address: "ул. Моршанская, д. 2",
         area: "1,6 м. кв.",
         glassType: "Осветленное стекло",
         price: "11 100",
         img: "c3.jpg"   
     }
-};
+]
