0012e0cab7032e53931980732e43a92f885220b4
dennis maftulyak <maft.den@gmail.com>
2018-12-13 08:36:06 +0200

    added slider switching by arrow

--- src/components/desktop/Image.js
+++ src/components/desktop/Image.js
@@ -1,57 +1,26 @@
 import React, { Component } from 'react'
 
 class Image extends Component {
-    // constructor(props) {
-    //     super(props);
-        
-    //     this.state = {
-    //         activeCat: 1,
-    //         preloadCat: []
-    //     };
-    // }
-
-    // preloadImg() {
-    //     Object.keys(clientInfo).map((el, i) => {
-    //         if (el.charAt(0) === this.context.first.toString()) {
-    //             const img = document.createElement('img');
-    //             img.src = './img/' + clientInfo[el].img;
-    //         }
-    //     });
-    // }
-
-    // componentWillMount() {
-    //     this.preloadImg();
-
-    //     this.setState(state => ({ 
-    //         activeCat: this.context.first,
-    //         preloadCat: [...state.preloadCat, this.context.first]
-    //     }));
-    // }
-
-    // componentDidUpdate() {
-    //     const { first } = this.context;
-    //     const { preloadCat, activeCat } = this.state;
-        
-    //     if (activeCat !== first && preloadCat.indexOf(first) === -1) {
-    //         this.preloadImg();
-
-    //         this.setState(state => ({ 
-    //             activeCat: first,
-    //             preloadCat: [...state.preloadCat, first]
-    //         }));
-    //     }
-    // }
-
     prevCategory = () => {
-        const { data, activeCategory } = this.props
+        const { data, activeCategory, toggleCategory } = this.props
 
-
+        for (let i = activeCategory.id - 2; i >= 0; i--) {
+            if (data[i].categoryName === activeCategory.name) {
+                toggleCategory(data[i].id, data[i].categoryName)
+                break
+            }
+        }
     }
 
     nextCategory = () => {
-        const { data, activeCategory } = this.props
+        const { data, activeCategory, toggleCategory } = this.props
 
-        
+        for (let i = activeCategory.id; i < data.length; i++) {
+            if (data[i].categoryName === activeCategory.name) {
+                toggleCategory(data[i].id, data[i].categoryName)
+                break
+            }
+        }
     }
     
     render() {
